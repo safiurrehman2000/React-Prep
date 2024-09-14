7845bdb74ace9002530a42bbf5b20dc4be1b18af
safiurrehman2000 <98805504+safiurrehman2000@users.noreply.github.com>
2024-09-14 20:48:50 +0500

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const useDebounce = (value = "", delay = 500) => {
+  const [debounce, setDebounce] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebounce(value);
+    }, delay);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+  return debounce;
+};
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        console.log(response);
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleFormChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(search);
+  }, [search]);
+
+  const searchedItems = data.filter((item) => {
+    return (
+      item.title.toLowerCase().includes(debouncedSearch.toLowerCase()) ||
+      item.body.toLowerCase().includes(debouncedSearch.toLowerCase())
+    );
+  });
+
+  if (loading) return <div>...loading</div>;
+  if (error) return <div>Error: {error.message}</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input
+          onChange={handleFormChange}
+          type="text"
+          name="search"
+          value={search}
+          placeholder="Search for a title"
+        />
+      </form>
+      <h1>Data</h1>
+      {searchedItems.length > 0 ? (
+        searchedItems.map((item, index) => (
+          <li style={{ listStyle: "none" }} key={index}>
+            <h3>Title: </h3>
+            {item.title}
+            <br />
+            <p>{item.body}</p>
+          </li>
+        ))
+      ) : (
+        <p>no items found</p>
+      )}
+    </>
   );
 }
-
-export default App;
